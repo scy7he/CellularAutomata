6f6727e62d1723480bd068b39710ca9bb5d60ab6
Sanvir Lala <sanvirl@gmail.com>
2017-08-30 05:22:30 +0200

    detect and count neighbours with nice debugging

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -6,21 +6,77 @@ using UnityEngine.UI;
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
 public class Room : MonoBehaviour {
-
   Vector3[] vertices;
-  public bool showIndices;
+  public bool showIndices, showNeighbourCount;
   Mesh mesh;
-
   Color debugColor;
+  bool alive;
+  int walls = 4;
+  Vector3 center;
+  float detectionRadius = 2f;
+  Vector3[] detectDirections;
 
-  void Awake() {
-  }
+  List<Room> neighbours;
+
+  bool isSelected = false;
 
   void Start() {
     mesh = GetComponent<MeshFilter>().mesh;
     vertices = mesh.vertices;
     debugColor = new Color(.9f, .3f, .55f, .8f);
+    detectDirections = InitDetectionVectors(walls * 2);
+    neighbours = new List<Room>(walls);
 
+    center = GetComponent<Renderer>().bounds.center;
+    
+    neighbours = findNeighbours(center, detectDirections);
+
+  }
+
+  List<Room> findNeighbours(Vector3 center, Vector3[] directions) {
+    List<Room> foundAdjacent = new List<Room>();
+
+    foreach(Vector3 dir in directions) {
+      Ray ray = new Ray(center, dir);
+      RaycastHit hit;
+      if (Physics.Raycast(ray, out hit, detectionRadius)) {
+        Room newNeighb = hit.collider.gameObject.GetComponent<Room>();
+        if ((newNeighb != null) && (!foundAdjacent.Contains(newNeighb))) {
+          foundAdjacent.Add(newNeighb);
+        }
+      }
+    }
+
+    return foundAdjacent;
+  }
+
+  public int GetAliveNeighbours() {
+    int alive = 0;
+    foreach (Room n in neighbours) {
+      if (n.alive) {
+        alive++;
+      }
+    }
+
+    return alive;
+  }
+
+  Vector3[] InitDetectionVectors(int wallCount) {
+    Vector3[] directions = new Vector3[wallCount];
+
+    Vector3 origin = Vector3.forward;
+    directions[0] = origin;
+
+    for (int i=1; i< wallCount; i++) {
+      Vector3 nextDirection = Quaternion.Euler(0, (360/wallCount) * i, 0) * origin;
+      directions[i] = nextDirection;
+    }
+
+    return directions;
+  }
+
+  public void SetAlive(bool newAlive) {
+    alive = newAlive;
   }
 
   void OnDrawGizmosSelected() {
@@ -31,6 +87,20 @@ public class Room : MonoBehaviour {
         Gizmos.DrawSphere(localPos, .1f);
       }
     }
+
+    if (detectDirections != null) {
+      Gizmos.color = Color.green;
+      foreach(Vector3 d in detectDirections) {
+        Gizmos.DrawRay(center, d);
+      }
+    }
+
+    foreach(Room r in neighbours) {
+      if (r.alive) {
+        Gizmos.DrawCube(r.center, Vector3.one * 1.2f);
+      }
+    }
+    isSelected = true;
   }
 
   void OnGUI() {
@@ -42,6 +112,17 @@ public class Room : MonoBehaviour {
         float invertedY = Screen.height - screenPosition.y;
         GUI.Label(new Rect(screenPosition.x, invertedY,75,25), i.ToString());
       }
+    }
+
+    if (showNeighbourCount && isSelected && neighbours != null) {
+      GUI.color = Color.white;
+      GUIStyle fontStyle = new GUIStyle();
+      fontStyle.fontSize = 50;
+      fontStyle.normal.textColor = Color.white;
+
+      Vector3 screenPosition = Camera.main.WorldToScreenPoint( center );
+      float invertedY = Screen.height - screenPosition.y;
+      GUI.Label(new Rect(screenPosition.x, invertedY,75,25), GetAliveNeighbours().ToString(), fontStyle);
     }
   }
 }
--- Assets/Scripts/RoomGenerator.cs
+++ Assets/Scripts/RoomGenerator.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum GenerationAlgorithm { MAZE = 0 };
+
 public class RoomGenerator : MonoBehaviour {
+  public GenerationAlgorithm method;
   public GameObject roomPrefab;
-  public Material cellMaterial;
+  public Material primaryMaterial, secondaryMaterial;
   public float roomScale;
   public bool showIndices;
-
   public Vector2 floorSize;
   List<GameObject> rooms;
+  float lastStepTime = 0f;
+
+  public bool[,] seed = {
+    {true, false, false, false, false, false, false, false, false, false },
+    {false, true, false, false, false, false, false, false, false, false },
+    {false, true, true, false, false, false, false, false, false, false },
+    {false, true, false, false, false, false, false, false, false, false },
+    {false, false, false, false, false, false, false, false, false, false },
+    {false, false, false, false, false, false, false, false, false, false },
+    {false, false, false, false, false, false, false, false, false, false },
+    {false, false, false, false, false, false, false, false, false, false },
+    {false, false, false, false, false, false, false, false, false, false },
+    {false, false, false, false, false, false, false, false, false, false },
+  };
 
   Vector3[] vertices = {
     Vector3.zero,
@@ -44,15 +60,34 @@ public class RoomGenerator : MonoBehaviour {
     for (int y=0; y<floorSize.y; y++) {
       for (int x=0; x<floorSize.x; x++) {
         Vector3 position = GetPositionAtIndex(x, y, floorSize, roomScale);
-        rooms.Add(CreateCubeMesh(vertices, triangles, roomScale, position));
+        rooms.Add(CreateCubeMesh(x, y, vertices, triangles, roomScale, position, seed[x,y]));
       }
     }
   }
 
-  GameObject CreateCubeMesh(Vector3[] verts, int[] tris, float scale, Vector3 position) {
-    GameObject newCell = Instantiate(roomPrefab, position, Quaternion.identity, transform) as GameObject;
+  void Update() {
 
-    Material newMaterial = (Material) Instantiate(cellMaterial);
+    if (Time.fixedTime - lastStepTime > 1f) {
+      switch(method) {
+        case GenerationAlgorithm.MAZE:
+          PerformRandom();
+          break;
+      }
+    }
+
+  }
+
+  void PerformRandom() {
+
+  }
+
+  GameObject CreateCubeMesh(int x, int y, Vector3[] verts, int[] tris, float scale, Vector3 position, bool alive) {
+    GameObject newCell = Instantiate(roomPrefab, position, Quaternion.identity, transform) as GameObject;
+    newCell.name = "Room "+x+" "+y;
+    newCell.GetComponent<Room>().SetAlive(alive);
+    newCell.GetComponent<Room>().showNeighbourCount = true;
+
+    Material newMaterial = (Material) Instantiate(alive ? primaryMaterial : secondaryMaterial);
     Material[] materials = newCell.GetComponent<Renderer>().materials;
     materials [0] = newMaterial;
     newCell.GetComponent<Renderer>().materials = materials;
@@ -67,6 +102,8 @@ public class RoomGenerator : MonoBehaviour {
     mesh.vertices = verts;
     mesh.triangles = tris;
     mesh.RecalculateNormals ();
+
+    newCell.GetComponent<MeshCollider>().sharedMesh = mesh;
 
     return newCell;
   }
